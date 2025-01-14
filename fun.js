f3379ab5e0ead88fcd555ff81a58dde449ca5275
Devansh Deshmukh <devanshd535@gmail.com>
2025-01-14 19:59:45 +0530

    done

--- fun.js
+++ fun.js
@@ -1,4 +1,17 @@
-let fruits = ["Apple", "Banana", "Cherry"];
-fruits.forEach(function(fruit) {
-    console.log(fruit);
-});
+    function printInfo(name, age){
+        console.log(`${name}'s age is' ${age}.`);
+    }
+    printInfo("Rohit",35);
+
+    //Create a function that returns the sum of numbers from 1 to n.
+    
+    function getSum(n){
+        let sum = 0;
+    
+    for(let i=1; i<=n; i++){
+        sum += i;  
+    }
+
+    return sum;
+
+}
