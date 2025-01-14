4830922f2556a835da6c4b18804ea8f0697eb751
Devansh Deshmukh <devanshd535@gmail.com>
2025-01-14 11:35:42 +0530

    complete

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let score = 85;
-if (score >= 90) {
-    console.log("A grade");
-} else if (score >= 80) {
-    console.log("B grade");
-} else {
-    console.log("Needs improvement");
-}
+// let score = 85;
+// if (score >= 90) {
+//     console.log("A grade");
+// } else if (score >= 80) {
+//     console.log("B grade");
+// } else {
+//     console.log("Needs improvement");
+// }
+let fruits = ["Apple", "Banana", "Cherry"];
+fruits.forEach(function(fruit) {
+    console.log(fruit);
+});
