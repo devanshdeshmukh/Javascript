8c193dbf1f55997fce475195428ec080a074f09b
devansh deshmukh <devanshd536@gmail.com>
2025-01-22 20:45:42 +0530

    commit

--- fun.js
+++ fun.js
@@ -5,13 +5,23 @@
 
     //Create a function that returns the sum of numbers from 1 to n.
     
-    function getSum(n){
-        let sum = 0;
+//     function getSum(n){
+//         let sum = 0;
     
-    for(let i=1; i<=n; i++){
-        sum += i;  
-    }
+//     for(let i=1; i<=n; i++){
+//         sum += i;  
+//     }
 
-    return sum;
+//     return sum;
 
+// }
+//
+// How do you iterate over an array?
+let arr = [10, 20, 30];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
+//How do you check if an array includes a value?
+let array = [1, 2, 3];
+console.log(array.includes(2)); 
+console.log(array.includes(4)); // 
