c9526730834806603f744ca0518385c8a46cd364
devansh deshmukh <devanshd536@gmail.com>
2025-01-31 00:26:42 +0530

    commit

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
-let color = "green";
+// let color = "green";
 
-// Traffic light system
-if(color === "red"){
-    console.log("Stop! light color is red");
-}
-else if(color === "yellow"){
-    console.log("Slow down. light color is yellow");
-}
-else if(color === "green"){
-    console.log("Go. light color is green");
-}
-else {
-    console.log("Traffic light is broken");
+// // Traffic light system
+// if(color === "red"){
+//     console.log("Stop! light color is red");
+// }
+// else if(color === "yellow"){
+//     console.log("Slow down. light color is yellow");
+// }
+// else if(color === "green"){
+//     console.log("Go. light color is green");
+// }
+// else {
+//     console.log("Traffic light is broken");
+// }
+let score = 75;
+let result;
+if (score >= 50) {
+    result = "Pass";
+} else {
+    result = "Fail";
 }
