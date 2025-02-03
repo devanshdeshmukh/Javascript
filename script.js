901d6a369f5fae74b680f4f6acb68389f4251987
devansh deshmukh <devanshd536@gmail.com>
2025-02-03 21:39:20 +0530

    second

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-//Methods = Actions that can be performed on an object.
+// //Methods = Actions that can be performed on an object.
 
-const calculator = {
-    num: 55,
-    add: function(a, b){
-        return a+b; 
-    },
-    sub: function(a, b){
-        return a-b; 
-    },
-    mul: function(a, b){
-        return a*b; 
-    }
+// const calculator = {
+//     num: 55,
+//     add: function(a, b){
+//         return a+b; 
+//     },
+//     sub: function(a, b){
+//         return a-b; 
+//     },
+//     mul: function(a, b){
+//         return a*b; 
+//     }
+// };
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(3, 5))
+
+// function sub(x, y){
+//     return x-y;
+// }
+// console.log(sub(11, 6))
+
+// function mul(i,j){
+//     return i*j;
+// }
+// console.log(mul(5, 10  ))
+
+//Arrow function 
+const sum = (a,b)=>{
+    console.log(a + b);
 };
 
-function sum(a,b){
-    return a+b;
+const cube = (n) =>{
+    return n*n*n;
 }
-console.log(sum(3, 5))
-
-function sub(x, y){
-    return x-y;
-}
-console.log(sub(11, 6))
-
-function mul(i,j){
-    return i*j;
-}
-console.log(mul(5, 10  ))
