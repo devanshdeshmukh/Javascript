b4dcd6cfb6197f451111048c5f17a77fac2402ac
devansh deshmukh <devanshd536@gmail.com>
2025-02-04 00:55:53 +0530

    commit

--- k.js
+++ k.js
@@ -9,3 +9,19 @@ let person = {
 };
 console.log(person.fullName());
 
+//Write an arrow function that returns thr square of number 'n'.
+ 
+const square = (n) => n*n;
+
+console.log(square(4));
+
+//Write  a function that prints "Hello World"5 times at intervals of 2s each.
+
+let id  = setInterval(() => {
+    console.log("Hello World");
+}, 2000); 
+
+setTimeout (() => {
+    clearInterval(id);
+    console.log("clear interval ran");
+}, 10000);
