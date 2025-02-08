1e3a94e37122005bc776ec0d372405261c7ecc31
devansh deshmukh <devanshd536@gmail.com>
2025-02-08 22:18:43 +0530

    commit

--- app.js
+++ app.js
@@ -62,3 +62,16 @@ const obj = {
       }
   }
 
+  //try and catch
+  console.log("hello");
+  console.log("hello");
+  let a = 5;
+  try {
+    console.log(a);
+  } catch(err) {
+    console.log("caught an error.. a is not defined");  
+    console.log(err);
+  }
+  console.log("hello2");
+  console.log("hello2");
+  console.log("hello2");
