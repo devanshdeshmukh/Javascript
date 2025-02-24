38509494407d790c37bc5d34ca185cd4668ae6be
devansh deshmukh <devanshd536@gmail.com>
2025-02-24 09:54:04 +0530

    second

---
+++ script.js
@@ -0,0 +1,8 @@
+let clutter = "";
+
+for(let i = 1; i<=152; i++){
+    let rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
