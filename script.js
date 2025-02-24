efddd7897f90b44f9c01717aed0455c16eae93a6
devansh deshmukh <devanshd536@gmail.com>
2025-02-24 20:14:14 +0530

    fourth

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let timer = 60;
+let score = 0;
+let hitrn = 0;
+
+function increaseScore(){
+    score  += 10;
+    document.querySelector("#scoreval").textContent = score; 
+}
 
 function getNewHit(){
-    let rn = Math.floor(Math.random()*10);
-    document.querySelector("#hitval").textContent = rn;
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
 }
 
 function makeBubble(){
@@ -24,10 +31,20 @@ function runTimer(){
         }
         else{
             clearInterval(timerint)
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
         }
-      
     }, 1000);
 }
+
+document.querySelector("#pbtm").addEventListener("click",
+     function(details){
+    let clickednum = Number(details.target.textContent)
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();    
+        getNewHit();
+    }
+  });
 
 runTimer();
 makeBubble();
