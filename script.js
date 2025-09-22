2db477ba35d27117cebc91559e1ff8aa3329cc90
devansh deshmukh <devanshd536@gmail.com>
2025-09-22 16:31:55 +0530

    datatypes

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+// let a = 10;
+// {
+//     let a = 20;
+//     console.log("inside", a);
+// }
+// console.log("outside", a);
+
+
+
+
+// Primitive
 let a = 10;
-{
-    let a = 20;
-    console.log("inside", a);
-}
-console.log("outside", a);
+let b = a;
+b = 20;a
+console.log(a); // 10 (unchanged)
+
+
+// Reference
+let obj1 = { name: "Devansh" };
+let obj2 = obj1;
+obj2.name = "Rahul";
+console.log(obj1.name); // Rahul (changed)
